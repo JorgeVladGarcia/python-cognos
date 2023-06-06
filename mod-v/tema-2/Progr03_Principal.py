7df89f3224a97a00361b187f025360ae7add8486
Jorge <35578115+JorgeVladGarcia@users.noreply.github.com>
2023-06-06 19:06:43 -0400

    add hw 2 solution

--- mod-v/tema-2/Progr03_Principal.py
+++ mod-v/tema-2/Progr03_Principal.py
@@ -2,3 +2,14 @@
 Progr03_Principal - Elaborar el programa principal que solicitará al usuario el departamento y el
 municipio, que desea consultar. Asimismo, llamará a los 3 módulos de acuerdo a la necesidad.
 '''
+
+from Progr02_Clase import mat_const
+
+munic = input("Introducir el municipio: ")
+dept = input("Introducir el departamento: ")
+
+xyz = mat_const()
+idp = xyz.mod_i(dept, munic)
+if len(idp) != 0:
+    total = xyz.mod_ii(dept, idp)
+    xyz.mod_iii(idp, total)
